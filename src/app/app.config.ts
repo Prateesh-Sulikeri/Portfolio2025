7f94bc8c3bdeec9f1a662d881bb7188c1b017b41
Prateesh-Sulikeri <sulikeriprateesh7@gmail.com>
2025-11-10 19:07:51 +0530

    scroll fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,4 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection
-} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,13 +6,12 @@ import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
       withInMemoryScrolling({
-        anchorScrolling: 'enabled',
-        scrollPositionRestoration: 'enabled'
+        anchorScrolling: 'disabled',          // 🔴 stop Angular from auto-scrolling to #anchors
+        scrollPositionRestoration: 'disabled' // 🔴 no auto restore
       })
     ),
     importProvidersFrom(HttpClientModule, FormsModule)
